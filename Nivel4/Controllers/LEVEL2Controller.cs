647b97cdf00b1993adad608fe8db839cfbc5eba0
lmonsalve <lmonsalve22@gmail.com>
2019-12-11 17:18:38 -0300

    level3 a medio camino

--- Nivel4/Controllers/LEVEL2Controller.cs
+++ Nivel4/Controllers/LEVEL2Controller.cs
@@ -50,10 +50,12 @@ namespace Nivel4.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ID_LEVEL2,ID_LEVEL1,NAME_LEVEL2")] LEVEL2 lEVEL2)
         {
+            lEVEL2.ID_LEVEL2 = db.LEVEL2.Count() + 1;
             if (ModelState.IsValid)
             {
                 db.LEVEL2.Add(lEVEL2);
                 db.SaveChanges();
+                db.Database.ExecuteSqlCommand("BEGIN LLENAR_MENU; END; ");
                 return RedirectToAction("Index");
             }
 
@@ -87,7 +89,21 @@ namespace Nivel4.Controllers
             if (ModelState.IsValid)
             {
                 db.Entry(lEVEL2).State = EntityState.Modified;
+                var list = db.LEVEL4.Where(c => c.LEVEL3.LEVEL2.ID_LEVEL2 == lEVEL2.ID_LEVEL2);
+                foreach (var item in list)
+                {
+                    LEVEL4 level4aux = db.LEVEL4.Find(item.ID_LEVEL4);
+                    LEVEL2 level2aux = db.LEVEL2.Find(item.LEVEL3.ID_LEVEL2);
+                    LEVEL1 level1aux = db.LEVEL1.Find(level2aux.ID_LEVEL1);
+                    level4aux.TAG = level2aux.LEVEL1.NAME_LEVEL1 + " " +
+                                     level2aux.NAME_LEVEL2 + " " +
+                                     level4aux.LEVEL3.NAME_LEVEL3 + " " +
+                                     level4aux.NAME_LEVEL4;
+                    db.Entry(level4aux).State = EntityState.Modified;
+
+                }
                 db.SaveChanges();
+                db.Database.ExecuteSqlCommand("BEGIN LLENAR_MENU; END; ");
                 return RedirectToAction("Index");
             }
             ViewBag.ID_LEVEL1 = new SelectList(db.LEVEL1, "ID_LEVEL1", "NAME_LEVEL1", lEVEL2.ID_LEVEL1);
@@ -116,7 +132,39 @@ namespace Nivel4.Controllers
         {
             LEVEL2 lEVEL2 = db.LEVEL2.Find(id);
             db.LEVEL2.Remove(lEVEL2);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                var lEVEL3 = db.LEVEL3.Include(l => l.LEVEL2).Where(c => c.LEVEL2.ID_LEVEL2 == lEVEL2.ID_LEVEL2);
+                //return View(    lEVEL4.ToListAsync());
+                ViewBag.Level2 = lEVEL3;
+                return View("Level3PorBorrar", lEVEL3.ToList());
+            }
+            db.Database.ExecuteSqlCommand("BEGIN LLENAR_MENU; END; ");
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Level3PorBorrar()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteAll(decimal id)
+        {
+            LEVEL2 lEVEL2 = db.LEVEL2.Find(id);
+            var lEVEL3 = db.LEVEL3.Where(c => c.LEVEL2.ID_LEVEL2 == lEVEL2.ID_LEVEL2);
+            List<LEVEL3> recorrido = lEVEL3.ToList();
+            foreach (var item in recorrido)
+            {
+                db.LEVEL3.Remove(item);
+            }
             db.SaveChanges();
+            db.LEVEL2.Remove(lEVEL2);
+            db.SaveChanges();
+            db.Database.ExecuteSqlCommand("BEGIN LLENAR_MENU; END; ");
             return RedirectToAction("Index");
         }
 
