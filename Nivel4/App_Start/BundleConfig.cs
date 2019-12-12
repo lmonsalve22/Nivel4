105d66167c61829a8c85f5acf64fc616276bd131
clentebanks <clentebanks0@gmail.com>
2019-12-12 01:58:04 -0600

    menu-level4

--- Nivel4/App_Start/BundleConfig.cs
+++ Nivel4/App_Start/BundleConfig.cs
@@ -9,7 +9,10 @@ namespace Nivel4
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-{version}.js",
+                        "~/Scripts/jquery-3.3.1.slim.min.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,11 +23,18 @@ namespace Nivel4
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/umd/popper.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/Content/js").Include(
+                       "~/Scripts/jquery-3.3.1.slim.min.js"
+                       ));
         }
     }
+    
 }
